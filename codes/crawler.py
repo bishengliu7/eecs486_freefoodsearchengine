532d5d20f6cb6769a3754caa60b4ae9a4efb9c43
kenelm007 <jessepan@umich.edu>
2017-04-18 16:21:46 -0400

    add crawler comment

--- codes/crawler.py
+++ codes/crawler.py
@@ -5,39 +5,15 @@ from urlparse import urljoin
 import requests
 import csv
 
-def parseURL(url, root):
-  # empty url
-  if not url or len(url) < 1:
-    return ""
-
-  # detect non-html
-  suffix = [".jpg", ".pdf", ".png", ".mp3", ".mp4", ".zip", ".wmv", ".mov", ".ppt", ".eps"]
-  if url[-4:].lower() in suffix:
-    return ""
-
-  if url[:4] != "http":
-    url = urljoin(root, url)
-  elif url[:5] == "https":
-    url = "http" + url[5:]
-
-  if url[-1] == '/':
-    url = url[:-1]
-
-  if url[:25] == "http://www.eecs.umich.edu":
-    return url
-  elif url[:21] == "http://eecs.umich.edu":
-    return "http://www.eecs.umich.edu" + url[21:]
-  else:
-    return ""
-
 if __name__ == "__main__":
+  # We only crawle URL that starts with base URL
   baseUrl = "http://events.umich.edu/event/"
 
+  # We open a csvfile to store crawled events
   csvfile = open('final_events.csv', 'wb')
   eventwriter = csv.writer(csvfile, delimiter=',')
   eventwriter.writerow(['id', 'title', 'desc', 'loc', 'date', 'tags'])
 
-  events = []
   for eventId in range(1,41000):
     url = baseUrl + str(eventId)
     try:
@@ -51,11 +27,9 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(r.text, "html.parser")
 
+    # discard event with no title
     if len(soup.findAll("h2", { "class" : "page-title" })) > 0:
       continue
-
-    # print (eventId, soup.findAll("h1", { "class" : "title" }))
-    # get title, description, location, date
 
     event = {'id': eventId,
              'title': "",
@@ -63,11 +37,14 @@ if __name__ == "__main__":
              'loc': "",
              'date': [],
              'tags': []}
+
+    # fetch event title
     if soup.findAll("h1", { "class" : "title" }):
       event['title'] = str(soup.findAll("h1", { "class" : "title" })[0].get_text().encode('utf-8'))
     else:
       continue
-      
+    
+    # fetch event description
     fetch = soup.findAll("div", { "class" : "event-description" })
     if fetch:
       event['desc'] = str(' '.join(fetch[0].get_text().encode('utf-8').split()))
@@ -76,20 +53,22 @@ if __name__ == "__main__":
     else:
       continue
 
+    # fetch event location
     fetch = soup.findAll("i", { "class" : "fa-location-arrow" })
     if fetch:
       event['loc'] = str(' '.join(fetch[0].parent.get_text().encode('utf-8').split()))
     
+    # fetch event time
     for i in soup.findAll("time"):
       event['date'].append(str(i['datetime']))
 
+    # fetch event tages
     for i in soup.findAll("i", { "class" : "fa-tags" }):
       event["tags"].append(str(i.parent.get_text().encode('utf-8').strip()))
 
-    print(eventId)
+    # print(eventId)
 
-    # events.append(event)
-
+    # write to csv file
     eventwriter.writerow([event['id'], event['title'], event['desc'], event['loc'],
        event['date'], event['tags']])
           
